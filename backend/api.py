eacbf17bce4d264fe19fefbb76d5de19c2d68599
ansh <sharmaansh8140@gmail.com>
2026-02-07 16:08:16 +0530

    adding a new feature of streaming in the backend

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from langchain_core.messages import HumanMessage,AIMessage
 from fastapi import FastAPI
 from uuid import uuid4
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 app.add_middleware(
@@ -17,20 +18,17 @@ app.add_middleware(
 class UserReply(BaseModel):
     message:str
     thread_id:str|None=None
-class llmResponse(BaseModel):
-    thread_id:str 
-    reply:str
 
-#my name is ansh
-#hey
-@app.post("/send",response_model=llmResponse)   #response_model is in built and is used to check whether the output coming is of schema llmresponse or not
+@app.post("/stream")
 def response(req:UserReply):
     thread_id=req.thread_id or str(uuid4())
-    result=workflow.invoke({"messages":[HumanMessage(req.message)]},config={"configurable":{"thread_id":thread_id}})
-
-    reply=result['messages'][-1].content
-
-    return{
-        "reply":reply,
-        "thread_id":thread_id
-    }
+#note that AImessage is a class and chunk is an object of that class and content is just a method 
+    def chunk_generator(): 
+        for chunk,metadata in workflow.stream({"messages":[HumanMessage(content=req.message)]}, config={"configurable":{"thread_id":thread_id}}, stream_mode="messages"): #langraph uses plural so dont mention message or etc  ---when this line hit and the langgraph_python trigger and start happen which make .invoje hit and give output in chatbot and that output containing aimessage is shown 
+            if isinstance(chunk,AIMessage) and chunk.content: #---then this line gets printed --- isinstance(obj,class) this is the main syntax
+                yield chunk.content
+    return StreamingResponse(
+        chunk_generator(),  # no need to write body= as the first positional arg is always a body
+        media_type="text/plain",
+        headers={"thread_id":thread_id}
+    )         
